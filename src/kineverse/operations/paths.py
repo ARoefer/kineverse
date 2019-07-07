5d0d9e1c32f8b3c733f3b9f9e4af314f6c169486
Adrian Röfer <aroefer@uni-bremen.de>
2019-07-07 22:16:26 +0200

    Data structures for using path-like keys in sets and dictionaries. Currently not in use

---
+++ src/kineverse/operations/paths.py
@@ -0,0 +1,57 @@
+
+
+class PathDict(object):
+    def __init__(self, value=None, paths=[]):
+        super(PathDict, self).__init__()
+        self.value = value
+        for p, v in paths:
+            self[p] = v
+
+    # True contains
+    def __contains__(self, key):
+        return len(key) == 0 or (super(PathDict, self).__contains__(key[0]) and key[1:] in super(PathDict, self).__getitem__(key[0]))
+
+    # Gets closest value!
+    def __getitem__(self, key):
+        return self.value if len(key) == 0 or not super(PathDict, self).__contains__(key[0]) else super(PathDict, self).__getitem__(key[0])[key[1:]]
+
+    # Gets closest value and returns its key too
+    def get_with_key(self, key):
+        if len(key) > 0:
+            k, v = t.get_with_key(key[1:])
+            return key[:1] + k, v
+        return tuple(), self.value
+
+    # Sets a value while creating the path to it
+    def __setitem__(self, key, value):
+        if len(key) == 0:
+            self.value = value
+        else:
+            if not super(PathDict, self).__contains__(key[0]):
+                super(PathDict, self).__setitem__(key[0], PathDict())
+            super(PathDict, self).__getitem__(key[0])[key[1:]] = value
+
+
+class PathSet(set):
+    # Tests whether a path or any of its prefixes are covered by this set
+    def __contains__(self, key):
+        for x in range(len(key)):
+            if super(PathSet, self).__contains__(key[:x + 1]):
+                return True
+        return False
+
+    # Tests whether the given key is a prefix of any element in the set
+    def is_prefix(self, key):
+        for p in self:
+            if p[:len(key)] == key:
+                return True
+        return False
+
+    # Test whether the exact key is in this set
+    def true_in(self, key):
+        return super(PathSet, self).__contains__(key)
+
+    def add(self, key):
+        if type(key) != tuple:
+            raise Exception('Path sets can only store tuples. Key: "{}" is of type {}.'.format(key, type(key)))
+        super(PathSet, self).add(key)
