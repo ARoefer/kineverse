ce05eb4e230104982b1266bfdd061605a3e85dc2
Adrian Röfer <aroefer@uni-bremen.de>
2019-07-09 12:08:14 +0200

    New operation interface

--- src/kineverse/operations/operation.py
+++ src/kineverse/operations/operation.py
@@ -1,20 +1,20 @@
+
+# Every action needs to declare the fields it depends on and the ones it modifies
 class Operation(object):
-    def __init__(self, name, t_exprs, t_args, **paths):
-        self.t_exprs = t_exprs
-        self.t_args  = t_args
-        self.o_exprs = {}
-        self.e_paths    = {k: (t, paths[k]) for k, t in self.t_exprs.items()}
-        self.args_paths = {k: (t, paths[k]) for k, t in self.t_args.items()}
+    def __init__(self, name, args, modifications, **kwargs):
+        self.args_paths = {a : kwargs[a] for a in args}
+        self.mod_paths  = {m : kwargs[m] for m in modifications}
+        self._memento   = {} # Memento of the modified expressions
 
     def apply(self, ks):
-        self.o_exprs = {k: (p, ks.get_typed_expr(p, t)) for k, (t, p) in self.e_paths.items()}
-        args = {k: ks.get_typed_expr(p, t) for k, (t, p) in self.args_paths.items()}
-        args.update({k: e for k, (_, e) in self.o_exprs.items()})
-        self._apply(**args)
+        self._memento = {m: ks.get_data(p) for m, p in self.mod_paths.items() if ks.has_data(p)}
+        self._apply(ks, **{k: e for k, e in self.mod_paths.items() + self.args_paths.items()})
 
-    def _apply(self, **kwargs):
+    def _apply(self, ks, **kwargs):
         raise NotImplementedError
 
     def revoke(self, ks):
-        for p, e in self.o_exprs.values():
-            ks.set_expr(p, e)
+        for p, e in self._memento.values():
+            ks.set_data(p, e)
+        for p in {p for k, p in self.mod_paths.items() if k not in self._memento}:
+            ks.remove_data(p)
