7c4695665d25b189f461011e82a609ffef9b2db3
Adrian Röfer <aroefer@uni-bremen.de>
2019-07-19 17:04:56 -0400

    Added visualization and gaze-goal to sandbox example.

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -1,3 +1,7 @@
+import rospy
+import subprocess
+import os
+
 from kineverse.gradients.gradient_math    import *
 from kineverse.model.kinematic_model      import KinematicModel, Path
 from kineverse.motion.integrator          import CommandIntegrator
@@ -6,20 +10,47 @@ from kineverse.motion.min_qp_builder      import SoftConstraint, ControlledValue
 from kineverse.operations.urdf_operations import load_urdf
 from kineverse.type_sets                  import atomic_types
 from kineverse.utils                      import res_pkg_path
-from kineverse.visualization.plotting     import draw_recorders
+from kineverse.visualization.plotting     import draw_recorders, split_recorders
+
+from sensor_msgs.msg     import JointState as JointStateMsg
 
 from urdf_parser_py.urdf import URDF
 
 
 if __name__ == '__main__':
+    rospy.init_node('kineverse_sandbox')
+
+    pub_path = '/opt/ros/{}/lib/robot_state_publisher/robot_state_publisher'.format(os.environ['ROS_DISTRO'])
+
+    with open(res_pkg_path('package://fetch_description/robots/fetch.urdf'), 'r') as urdf_file:
+        urdf_str = urdf_file.read()
+
+    js_pub = rospy.Publisher('/joint_states', JointStateMsg, queue_size=1)
+    rospy.set_param('/robot_description', urdf_str)
+    urdf_model = URDF.from_xml_string(urdf_str)
+
+    # ROBOT STATE PUBLISHER
+    sp_p = subprocess.Popen([pub_path,
+                            '__name:={}_state_publisher'.format(urdf_model.name),
+                            'robot_description:=/robot_description',
+                            '_tf_prefix:={}'.format(urdf_model.name),
+                            'joint_states:=/joint_states'])
+
     km = KinematicModel()
-    load_urdf(km, Path('fetch'), URDF.from_xml_file(res_pkg_path('package://fetch_description/robots/fetch.urdf')))
+    load_urdf(km, Path('fetch'), urdf_model)
 
 
     eef_pose = km.get_data('fetch/links/gripper_link/pose')
     eef_pos  = pos_of(eef_pose)
 
-    goal = point3(1, 0.3, 1)
+    cam_pose    = km.get_data('fetch/links/head_camera_link/pose')
+    cam_pos     = pos_of(cam_pose)
+    cam_forward = x_of(cam_pose)
+    cam_to_eef  = eef_pos - cam_pos
+
+    look_goal = 1 - (dot(cam_to_eef, cam_forward) / norm(cam_to_eef))
+
+    goal = point3(0.4, 0.4, 1.4)
     dist = norm(goal - eef_pos)
 
     joint_symbols = [j.position for j in km.get_data('fetch/joints').values() if hasattr(j, 'position') and type(j.position) is spw.Symbol]
@@ -40,14 +71,31 @@ if __name__ == '__main__':
             controlled_values[str(s)] = ControlledValue(-1e9, 1e9, s, 0.01)
 
 
-    goal_constraints = {'reach_point': SoftConstraint(-dist, -dist, 1, dist)}
+    goal_constraints = {'reach_point': SoftConstraint(-dist, -dist, 1, dist),
+                        'look_at_eef': SoftConstraint(-look_goal, -look_goal, 1, look_goal)}
 
-    integrator = CommandIntegrator(TQPB(constraints, goal_constraints, controlled_values), recorded_terms={'distance': dist})
+    integrator = CommandIntegrator(TQPB(constraints, goal_constraints, controlled_values), recorded_terms={'distance': dist,
+                                            'gaze_align': look_goal})
 
+    int_factor = 0.02
     integrator.restart('Fetch Cartesian Goal Example')
-    integrator.run(0.05)
+    integrator.run(int_factor)
 
-    draw_recorders([integrator.recorder, integrator.sym_recorder], 4.0/9.0, 8, 4).savefig(res_pkg_path('package://kineverse/test/plots/fetch_sandbox_plots.png'))
+    draw_recorders([integrator.recorder] + split_recorders([integrator.sym_recorder]), 4.0/9.0, 8, 4).savefig(res_pkg_path('package://kineverse/test/plots/fetch_sandbox_plots.png'))
 
+    trajectory = {Path(j)[-1][:-2]: [0.0] * len(integrator.recorder.data.values()[0]) for j in joint_symbols}
+    trajectory.update({Path(spw.Symbol(j))[-1][:-2]: d for j, d in integrator.recorder.data.items()})
+    x = 0
+    jsmsg      = JointStateMsg()
+    jsmsg.name = trajectory.keys()
+    while not rospy.is_shutdown() and x < len(trajectory.values()[0]):
+        now = rospy.Time.now()
+        if (now - jsmsg.header.stamp).to_sec() >= int_factor:
+            jsmsg.header.stamp = now
+            jsmsg.position = [trajectory[j][x] for j in jsmsg.name] 
+            js_pub.publish(jsmsg)
+            x += 1 
 
+    sp_p.terminate()
+    sp_p.wait()
 
