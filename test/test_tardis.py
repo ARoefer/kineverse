883aeb7ec0c40fdb716f5de85c9a0da817b6aad9
Adrian Röfer <aroefer@uni-bremen.de>
2019-07-19 18:20:36 -0400

    Added wrapper function, which wraps subclasses of KinematicModel to track operation executions in real time and generate past models.

---
+++ test/test_tardis.py
@@ -0,0 +1,14 @@
+import unittest as ut
+
+from kineverse.model.kinematic_model       import KinematicModel, Path
+from kineverse.operations.basic_operations import CreateComplexObject
+from kineverse.model.tardis_wrapper        import TARDIS
+
+class TestTARDIS(ut.TestCase):
+
+    def test_tardis(self):
+        pass
+
+
+if __name__ == '__main__':
+    ut.main()
