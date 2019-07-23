088b53ccdc6de44158ae4f69913e1cf28c872f42
Adrian Röfer <aroefer@uni-bremen.de>
2019-07-23 15:03:14 -0400

    Removed debug-printing and added coloring in dependency graph generator.

--- src/kineverse/visualization/graph_generator.py
+++ src/kineverse/visualization/graph_generator.py
@@ -8,12 +8,16 @@ from kineverse.model.kinematic_model import KinematicModel
 def generate_modifications_graph(km):
     pass
 
-def generate_dependency_graph(km):
+def get_color(name, colors):
+    for prefix, c in colors.items():
+        if name[:len(prefix)] == prefix:
+            return c
+    return 'black'
+
+def generate_dependency_graph(km, prefix_colors={}):
     sorted_tags = sorted([(t, tag) for tag, t in km.timeline_tags.items()])
-    nodes_str = ';\n    '.join(['t_{} [label="{}"]'.format(t, tag) for t, tag in sorted_tags])
+    nodes_str = ';\n    '.join(['t_{} [label="{}",color={}]'.format(t, tag, get_color(tag, prefix_colors)) for t, tag in sorted_tags])
     edges_str = ';\n    '.join([';\n    '.join(['t_{} -> t_{}'.format(d.stamp, c.stamp) for d in c.dependents]) for c in km.operation_history.chunk_history if len(c.dependents) > 0])
-    print(nodes_str)
-    print(edges_str)
     return 'digraph dep_graph {{\n    {};\n    {}\n}}'.format(nodes_str, edges_str)
 
 
