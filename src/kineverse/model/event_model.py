eb9544f2c9186b5be5fac0b16017e21430f53dca
Adrian Röfer <aroefer@uni-bremen.de>
2019-07-26 12:21:31 -0400

    Added EventModel, a subclass of KinematicModel adding event generation and dispatching to model modifications.

---
+++ src/kineverse/model/event_model.py
@@ -0,0 +1,183 @@
+import re
+
+from kineverse.model.kinematic_model import KinematicModel
+from kineverse.model.paths           import Path, PathDict, PathSet
+    
+def _dispatch_model_events(pdict, data, key, call_tracker=set()):
+    for cb in pdict.value:
+        if cb not in call_tracker:
+            cb(data)
+            call_tracker.add(cb)
+    if len(key) == 0:
+        for k, p in pdict.items():
+            _dispatch_model_events(p, Path(k).get_data_no_throw(data), key, call_tracker)
+    else:
+        _dispatch_model_events(pdict.get_sub_dict(key[0]), key[:1].get_data_no_throw(data), key[1:], call_tracker)
+
+
+class EventModel(KinematicModel):
+    def __init__(self):
+        super(EventModel, self).__init__()
+        self.model_change_callbacks    = PathDict(set(), default_factory=set)
+        self.constraint_callbacks      = {}
+        # Inverse mapping of callbacks to their triggers, for easier removal 
+        self._callback_path_registry   = {}
+        self._callback_symbol_registry = {}
+
+        self._constraint_callback_batch = {}
+        self._constraint_discard_batch  = {}
+        self._callback_batch = PathSet()
+
+        self.tag_callbacks = {}
+        self._callback_matcher_registry = {}
+
+    def apply_operation(self, op, tag):
+        super(EventModel, self).apply_operation(op, tag)
+        self._on_operation_changed(tag, True)
+
+    def apply_operation_before(self, op, tag, before_tag):
+        super(EventModel, self).apply_operation_before(op, tag, before_tag)
+        self._on_operation_changed(tag, True)
+
+    def apply_operation_after(self, op, tag, after_tag):
+        super(EventModel, self).apply_operation_after(op, tag, after_tag)
+        self._on_operation_changed(tag, True)
+
+    def remove_operation(self, tag):
+        super(EventModel, self).remove_operation(tag)
+        self._on_operation_changed(tag, False)
+
+    def _on_operation_changed(self, tag, applied):
+        call_tracker = set()
+        for m, cbs in self.tag_callbacks.items():
+            if m.match(tag) is not None:
+                for cb in cbs:
+                    if cb not in call_tracker:
+                        cb(tag, applied)
+                        call_tracker.add(cb)
+
+
+    def set_data(self, key, value):
+        key = Path(key) if type(key) == str else key
+        if not self.has_data(key) or value != self.get_data(key):
+            self._callback_batch.add(key)
+        super(EventModel, self).set_data(key, value)
+
+    def remove_data(self, key):
+        key = Path(key) if type(key) == str else key
+        if self.has_data(key):
+            self._callback_batch.add(key)
+        super(EventModel, self).remove_data(key)
+
+    def add_constraint(self, key, constraint):
+        if key not in self._constraint_callback_batch:
+            self._constraint_callback_batch[key] = set()
+        
+        if key in self.constraints:
+            for s in self.constraints[key].expr.free_symbols:
+                if s in self.constraint_callbacks and s not in constraint.expr.free_symbols:
+                    for cb in self.constraint_callbacks[s]:
+                        if cb not in self._constraint_discard_batch:
+                            self._constraint_discard_batch[cb] = set()
+                        self._constraint_discard_batch[cb].add(key)
+
+        super(EventModel, self).add_constraint(key, constraint)
+        for s in constraint.expr.free_symbols:
+            if s in self.constraint_callbacks:
+                self._constraint_callback_batch[key].update(self.constraint_callbacks[s])        
+
+    def remove_constraint(self, key):    
+        if key in self.constraints:
+            if key not in self._constraint_callback_batch:
+                self._constraint_callback_batch[key] = set()
+
+            for s in self.constraints[key].expr.free_symbols:
+                if s in self.constraint_callbacks:
+                    self._constraint_callback_batch[key].update(self.constraint_callbacks[s])
+        super(EventModel, self).remove_constraint(key)
+
+
+    def clean_structure(self, until=2e9):
+        super(EventModel, self).clean_structure(until)
+
+
+    def register_on_operation_changed(self, pattern, callback):
+        matcher = re.compile(pattern)
+        if matcher not in self.tag_callbacks:
+            self.tag_callbacks[matcher] = set()
+        self.tag_callbacks[matcher].add(callback)
+
+        if callback not in self._callback_matcher_registry:
+            self._callback_matcher_registry[callback] = set()
+        self._callback_matcher_registry[callback].add(matcher)
+
+
+    def deregister_on_operation_changed(self, callback):
+        if callback in self._callback_matcher_registry:
+            for m in self._callback_matcher_registry[callback]:
+                self.tag_callbacks[m].remove(callback)
+            del self._callback_matcher_registry[callback]
+
+
+    def register_on_model_changed(self, path, callback):
+        if path not in self.model_change_callbacks:
+            self.model_change_callbacks[path] = set()
+        self.model_change_callbacks[path].add(callback)
+        
+        if callback not in self._callback_path_registry:
+            self._callback_path_registry[callback] = set()
+        self._callback_path_registry[callback].add(path)
+
+
+    def deregister_on_model_changed(self, callback):
+        if callback in self._callback_path_registry:
+            for p in self._callback_path_registry[callback]:
+                self.model_change_callbacks[p].remove(callback)
+            del self._callback_path_registry[callback]
+
+
+    def register_on_constraints_changed(self, symbols, callback):
+        for s in symbols:
+            if s not in self.constraint_callbacks:
+                self.constraint_callbacks[s] = set()
+            self.constraint_callbacks[s].add(callback)
+
+        if callback not in self._callback_symbol_registry:
+            self._callback_symbol_registry[callback] = set()
+        self._callback_symbol_registry[callback].update(symbols)
+
+
+    def deregister_on_constraints_changed(self, callback):
+        if callback in self._callback_symbol_registry:
+            for s in self._callback_symbol_registry:
+                self.constraint_callbacks[s].remove(callback)
+            del self._callback_symbol_registry[callback]
+
+
+    def dispatch_events(self):
+        # Model Updates
+        call_tracker = set()
+
+        for p in self._callback_batch:
+            if p[0] in self.model_change_callbacks:
+                _dispatch_model_events(self.model_change_callbacks.get_sub_dict(p[0]), 
+                                       p[:1].get_data_no_throw(self.data_tree.data_tree),
+                                       p[1:], call_tracker)
+
+        # Constraint Updates
+        for k, cbs in self._constraint_callback_batch.items():
+            if len(cbs) > 0:
+                c = self.constraints[k] if k in self.constraints else None
+                for cb in cbs:
+                    cb(k, c)
+                    if cb in self._constraint_discard_batch:
+                        self._constraint_discard_batch[cb].discard(k)
+
+        for cb, keys in self._constraint_discard_batch.items():
+            for k in keys:
+                cb(k, None)
+
+        # Clean-up
+        self._callback_batch = PathSet()
+        self._constraint_callback_batch = {}
+        self._constraint_discard_batch  = {}
---
+++ test/test_events.py
@@ -0,0 +1,243 @@
+import unittest as ut
+from kineverse.gradients.gradient_math import spw
+from kineverse.model.event_model       import EventModel, Path
+from kineverse.model.kinematic_model   import Constraint
+from kineverse.operations.operation    import Operation
+from kineverse.utils                   import bb
+
+
+class EmptyOperation(Operation):
+    def __init__(self):
+        super(EmptyOperation, self).__init__('Empty Operation', [])
+
+    def _apply(self, ks):
+        return {}, {}
+
+
+class TestEventModel(ut.TestCase):
+
+    def test_modifcation_events(self):
+        km = EventModel()
+
+        self.change_a = None
+        self.change_b = None
+        self.change_c = None
+        self.change_c_f = None
+        self.change_c_g = None
+        def cb_change_a(data):
+            self.change_a = data
+
+        def cb_change_b(data):
+            self.change_b = data
+
+        def cb_change_c(data):
+            self.change_c = data
+
+        def cb_change_c_f(data):
+            self.change_c_f = data
+
+        def cb_change_c_g(data):
+            self.change_c_g = data
+
+        km.register_on_model_changed(Path('a'), cb_change_a)
+        km.register_on_model_changed(Path('b'), cb_change_b)
+        km.register_on_model_changed(Path('c'), cb_change_c)
+        km.register_on_model_changed(Path('c/f'), cb_change_c_f)
+        km.register_on_model_changed(Path('c/g'), cb_change_c_g)
+
+        km.set_data('a', 5)
+        km.dispatch_events()
+        self.assertEquals(self.change_a, 5)
+        self.assertEquals(self.change_b, None)
+
+        km.set_data('b', 6)
+        km.dispatch_events()
+        self.assertEquals(self.change_b, 6)
+        self.assertEquals(self.change_c, None)
+
+        self.change_b = None
+        km.set_data('b', 6)
+        km.dispatch_events()
+        self.assertEquals(self.change_b, None)
+
+        c_1 = bb(f='lol', g=False)
+        c_2 = bb(f='kek', g=False)
+        c_3 = bb(f='foo', g=True)
+        km.set_data('c', c_1)
+        km.dispatch_events()
+        self.assertEquals(self.change_c, c_1)
+        self.assertEquals(self.change_c_f, 'lol')
+        self.assertEquals(self.change_c_g, False)
+
+        self.change_c = None
+        self.change_c_g = None
+
+        km.set_data('c/f', 'kek')
+        km.dispatch_events()
+        self.assertEquals(self.change_c, c_1)
+        self.assertEquals(self.change_c_f, 'kek')
+        self.assertEquals(self.change_c_g, None)
+
+        km.set_data('c', c_3)
+        km.dispatch_events()
+        self.assertEquals(self.change_c, c_3)
+        self.assertEquals(self.change_c_f, 'foo')
+        self.assertEquals(self.change_c_g, True)
+
+        km.remove_data('c')
+        km.dispatch_events()
+        self.assertEquals(self.change_c, None)
+        self.assertEquals(self.change_c_f, None)
+        self.assertEquals(self.change_c_g, None)
+
+        self.change_b = None
+        km.deregister_on_model_changed(cb_change_b)
+        km.set_data('b', 42)
+        km.dispatch_events()
+        self.assertEquals(self.change_b, None)
+
+
+    def test_constraint_changed(self):
+        km = EventModel()
+
+        s_a_constraints = {}
+        s_b_constraints = {}
+        s_c_constraints = {}
+        s_d_constraints = {}
+        s_ba_constraints = {}
+        s_dcb_constraints = {}
+
+        def gen_cb(*cb_dicts):
+            def cb(name, c):
+                for cbd in cb_dicts:
+                    if c is not None:
+                        cbd[name] = c
+                    elif name in cbd:
+                        del cbd[name]
+
+            return cb
+
+        s_a, s_b, s_c, s_d = spw.sp.symbols('a b c d')
+
+        km.register_on_constraints_changed({s_a}, gen_cb(s_a_constraints))
+        km.register_on_constraints_changed({s_b}, gen_cb(s_b_constraints))
+        km.register_on_constraints_changed({s_c}, gen_cb(s_c_constraints))
+        km.register_on_constraints_changed({s_d}, gen_cb(s_d_constraints))
+        km.register_on_constraints_changed({s_a, s_b}, gen_cb(s_ba_constraints))
+        km.register_on_constraints_changed({s_d, s_c, s_b}, gen_cb(s_dcb_constraints))
+
+        c1 = Constraint(-1, 1, s_a)
+        c2 = Constraint(-1, 1, s_a * s_b)
+        c3 = Constraint(-1, 1, s_c)
+        c4 = Constraint(-1, 1, s_d * s_c * s_b)
+
+        # Insert constraint based on single variable
+        km.add_constraint('c1', c1)
+        km.dispatch_events()
+        self.assertIn('c1', s_a_constraints)
+        self.assertIn('c1', s_ba_constraints)
+        self.assertEquals(s_a_constraints['c1'],  c1)
+        self.assertEquals(s_ba_constraints['c1'], c1)
+        self.assertNotIn('c1', s_b_constraints)
+        self.assertNotIn('c1', s_c_constraints)
+        self.assertNotIn('c1', s_d_constraints)
+        self.assertNotIn('c1', s_dcb_constraints)
+
+        # Insert constraint based on different single variable
+        km.add_constraint('c3', c3)
+        km.dispatch_events()
+        self.assertIn('c3', s_c_constraints)
+        self.assertIn('c3', s_dcb_constraints)
+        self.assertEquals(s_c_constraints['c3'],  c3)
+        self.assertEquals(s_dcb_constraints['c3'], c3)
+        self.assertNotIn('c3', s_a_constraints)
+        self.assertNotIn('c3', s_b_constraints)
+        self.assertNotIn('c3', s_d_constraints)
+        self.assertNotIn('c3', s_ba_constraints)
+
+        # Insert constraint based on two variables
+        km.add_constraint('c2', c2)
+        km.dispatch_events()
+        self.assertIn('c2', s_a_constraints)
+        self.assertIn('c2', s_b_constraints)
+        self.assertIn('c2', s_ba_constraints)
+        self.assertIn('c2', s_dcb_constraints)
+        self.assertEquals(s_a_constraints['c2'],  c2)
+        self.assertEquals(s_b_constraints['c2'],  c2)
+        self.assertEquals(s_ba_constraints['c2'],  c2)
+        self.assertEquals(s_dcb_constraints['c2'], c2)
+        self.assertNotIn('c2', s_c_constraints)
+        self.assertNotIn('c2', s_d_constraints)
+
+        # Replace constraint, while changing its symbol set
+        km.add_constraint('c2', c4)
+        km.dispatch_events()
+        self.assertNotIn('c2', s_a_constraints)
+        self.assertIn('c2', s_c_constraints)
+        self.assertIn('c2', s_d_constraints)
+        self.assertIn('c2', s_b_constraints)
+        self.assertIn('c2', s_dcb_constraints)
+        self.assertEquals(s_c_constraints['c2'],  c4)
+        self.assertEquals(s_b_constraints['c2'],  c4)
+        self.assertEquals(s_d_constraints['c2'],  c4)
+        self.assertEquals(s_dcb_constraints['c2'],  c4)
+
+        # Remove constraint
+        km.remove_constraint('c2')
+        km.dispatch_events()
+        self.assertNotIn('c2', s_c_constraints)
+        self.assertNotIn('c2', s_d_constraints)
+        self.assertNotIn('c2', s_b_constraints)
+        self.assertNotIn('c2', s_dcb_constraints)
+
+
+    def test_operation_changed(self):
+        km = EventModel()
+
+        self.op_tracker = {}
+
+        def cb_op(tag, applied):
+            self.op_tracker[tag] = applied
+
+        km.register_on_operation_changed('foo', cb_op)
+        km.register_on_operation_changed('bar', cb_op)
+        km.register_on_operation_changed('kek \w*', cb_op)
+        km.register_on_operation_changed('hah \w* something', cb_op)
+        km.register_on_operation_changed('hah lol \w*', cb_op)
+
+        km.apply_operation(EmptyOperation(), 'nothing')
+        km.dispatch_events()
+        self.assertNotIn('nothing', self.op_tracker)
+
+        km.apply_operation(EmptyOperation(), 'foo')
+        km.dispatch_events()
+        self.assertIn('foo', self.op_tracker)
+        self.assertTrue(self.op_tracker['foo'])
+        
+        km.remove_operation('foo')
+        km.dispatch_events()
+        self.assertIn('foo', self.op_tracker)
+        self.assertFalse(self.op_tracker['foo'])
+
+        km.apply_operation(EmptyOperation(), 'kek')
+        km.dispatch_events()
+        self.assertNotIn('kek', self.op_tracker)
+
+        km.apply_operation(EmptyOperation(), 'kek wonderful')
+        km.dispatch_events()
+        self.assertIn('kek wonderful', self.op_tracker)
+        self.assertTrue(self.op_tracker['kek wonderful'])
+
+        km.apply_operation_before(EmptyOperation(), 'hah abc something', 'kek')
+        km.dispatch_events()
+        self.assertIn('hah abc something', self.op_tracker)
+        self.assertTrue(self.op_tracker['hah abc something'])        
+
+        km.apply_operation_after(EmptyOperation(), 'hah lol nothing', 'kek wonderful')
+        km.dispatch_events()
+        self.assertIn('hah lol nothing', self.op_tracker)
+        self.assertTrue(self.op_tracker['hah lol nothing'])
+
+
+if __name__ == '__main__':
+    ut.main()
