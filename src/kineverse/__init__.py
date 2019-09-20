5d78842b1fd967cdab59bdfa7f71bee32f54a5db
Adrian Röfer <aroefer@uni-bremen.de>
2019-09-20 16:25:01 +0200

    Removed YAML serializer. Added decorator for type checking

--- src/kineverse/__init__.py
+++ src/kineverse/__init__.py
@@ -5,3 +5,20 @@ except NameError:
         return f
     import __builtin__ as builtins
     builtins.__dict__['profile'] = profile
+
+TYPE_CHECKING = True
+
+def type_check(*types):
+    def dec_check(f):
+        if TYPE_CHECKING:
+            def checked_f(*args):
+                args_start = 0 if f.func_code.co_varnames[0] != 'self' else 1
+                if min([isinstance(v, t) for t, v in zip(types[:len(args) - args_start], args[args_start:])]) is False:
+                    raise Exception('Function {} called with wrong types:\n  Signature: {}\n      Given: {}'.format(f.func_name, ', '.join([str(t) for t in types[:len(args) - args_start]]), ', '.join([str(type(a)) for a in args[args_start:]])))
+                return f(*args)
+            return checked_f
+        else:
+            return f
+    return dec_check
+
+builtins.__dict__['type_check'] = type_check
