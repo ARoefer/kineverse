d38171f97d48b04ba4626b421104f68e5ff26838
Adrian Röfer <aroefer@uni-bremen.de>
2019-10-30 12:54:56 +0100

    Update to microwave sandbox and urdf.

--- scripts/microwave_sandbox.py
+++ scripts/microwave_sandbox.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
-from kineverse.gradients.diff_logic       import get_diff_symbol
-from kineverse.gradients.gradient_math    import *
-from kineverse.model.event_model          import EventModel
-from kineverse.model.geometry_model       import GeometryModel, Path, Constraint
-from kineverse.operations.urdf_operations import load_urdf
-from kineverse.utils                      import res_pkg_path
+import rospy
 
-from kineverse.motion.min_qp_builder      import TypedQPBuilder  as TQPB, \
-                                                 SoftConstraint  as SC, \
-                                                 ControlledValue as CV, \
-                                                 generate_controlled_values
-from kineverse.motion.integrator          import CommandIntegrator
-from kineverse.visualization.plotting     import draw_recorders, split_recorders
+from kineverse.gradients.diff_logic                import get_diff_symbol
+from kineverse.gradients.gradient_math             import *
+from kineverse.model.event_model                   import EventModel
+from kineverse.model.geometry_model                import GeometryModel, Path, Constraint
+from kineverse.operations.urdf_operations          import load_urdf
+from kineverse.utils                               import res_pkg_path
+
+from kineverse.motion.min_qp_builder               import TypedQPBuilder  as TQPB, \
+                                                          SoftConstraint  as SC, \
+                                                          ControlledValue as CV, \
+                                                          generate_controlled_values
+from kineverse.motion.integrator                   import CommandIntegrator
+from kineverse.visualization.bpb_visualizer        import ROSBPBVisualizer
+from kineverse.visualization.plotting              import draw_recorders, split_recorders
+from kineverse.visualization.trajectory_visualizer import TrajectoryVisualizer
 
 from urdf_parser_py.urdf import URDF 
 
 if __name__ == '__main__':
+    rospy.init_node('microwave_sandbox')
 
     micro_urdf = URDF.from_xml_file(res_pkg_path('package://kineverse/urdf/microwave.urdf'))
 
@@ -23,14 +28,17 @@ if __name__ == '__main__':
     #km = GeometryModel()
     load_urdf(km, Path('microwave'), micro_urdf)
 
-    door_position   = km.get_data('microwave/joints/door_joint').position
-    button_position = km.get_data('microwave/joints/button_joint').position
-    button_min      = -0.02
-    door_velocity   = get_diff_symbol(door_position)
-    door_accel      = get_diff_symbol(door_velocity)
+    traj_vis = TrajectoryVisualizer(ROSBPBVisualizer('/bullet_test', '/map'))
+    traj_vis.add_articulated_object(micro_urdf, km.get_data('microwave'))
+
+    door_position    = km.get_data('microwave/joints/door_joint').position
+    button_position  = km.get_data('microwave/joints/button_joint').position
+    button_min       = -0.02
+    door_velocity    = get_diff_symbol(door_position)
+    door_accel       = get_diff_symbol(door_velocity)
     point_of_release = 0.8 * button_min
-    button_pushed   = less_than(button_position, point_of_release)
-    condition       = alg_and(less_than(door_position, 0.1), button_pushed)
+    button_pushed    = less_than(button_position, point_of_release)
+    condition        = alg_and(less_than(door_position, 0.1), button_pushed)
 
     spring_constraint = Constraint((2 * condition) ** (-100 * door_position), 1e9, door_accel)
     km.add_constraint('spring_constraint', spring_constraint)
@@ -45,9 +53,12 @@ if __name__ == '__main__':
     goal = 1.57 - door_position
 
     controlled_symbols = {get_diff_symbol(button_position), door_velocity}
-    cvs, constraints = generate_controlled_values(km.get_constraints_by_symbols(controlled_symbols.union({button_position})), controlled_symbols)
+    constraints        = km.get_constraints_by_symbols(controlled_symbols.union({button_position}))
+    print('----\n{}'.format('\n'.join([str(x) for x in constraints.values()])))
+    
+    cvs, constraints = generate_controlled_values(constraints, controlled_symbols)
 
-    print('Retrieved Constraints:\n {}'.format('\n '.join(['{}:\n  {}'.format(k, c) for k, c in constraints.items()])))
+    print('Retrieved Constraints:\n{}'.format('\n'.join(['{}:\n  {}'.format(k, c) for k, c in constraints.items()])))
 
     integrator = CommandIntegrator(TQPB(constraints, 
                                         {'goal': SC(-0.1, -0.1, 1, button_position)},
@@ -64,3 +75,6 @@ if __name__ == '__main__':
 
     plot_dir = res_pkg_path('package://kineverse/test/plots')
     draw_recorders([integrator.recorder] + split_recorders([integrator.sym_recorder]), 4.0/9.0, 8, 4).savefig('{}/microwave_spring.png'.format(plot_dir))
+
+    traj_vis.visualize(integrator.recorder.data)
+    traj_vis.shutdown()
