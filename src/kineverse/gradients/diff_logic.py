4bfff36d648756cdfdf3e56303ae1b887dcc41a9
Simon Stelter <stelter@uni-bremen.de>
2020-07-18 09:10:03 +0200

    changed suffixes in difflogic

--- src/kineverse/gradients/diff_logic.py
+++ src/kineverse/gradients/diff_logic.py
@@ -4,23 +4,25 @@ The diff_logic module implements Kineverse's typed symbol system.
 
 import kineverse.gradients.common_math as cm
 
+magic_character = '__'
+
 TYPE_UNKNOWN  = 0
 TYPE_POSITION = 1
 TYPE_VELOCITY = 2
 TYPE_ACCEL    = 3
 TYPE_JERK     = 4
 TYPE_SNAP     = 5
-TYPE_SUFFIXES = {'_p': TYPE_POSITION, 
-                 '_v': TYPE_VELOCITY, 
-                 '_a': TYPE_ACCEL,
-                 '_j': TYPE_JERK,
-                 '_s': TYPE_SNAP}
+TYPE_SUFFIXES = {'position': TYPE_POSITION,
+                 'velocity': TYPE_VELOCITY,
+                 'acceleration': TYPE_ACCEL,
+                 'jerk': TYPE_JERK,
+                 'snap': TYPE_SNAP}
 TYPE_SUFFIXES_INV = {v: k for k, v in TYPE_SUFFIXES.items()}
 
 Symbol = cm.Symbol
 
 class CastException(Exception):
-    pass    
+    pass
 
 def create_symbol(symbol, stype):
     """Adds proper type suffix to the given symbol.
@@ -37,7 +39,7 @@ def create_symbol(symbol, stype):
 
     if stype not in TYPE_SUFFIXES_INV:
         raise Exception('Can not create symbol for type {}: Type id not defined.'.format(stype))
-    return cm.Symbol('{}{}'.format(str(symbol), TYPE_SUFFIXES_INV[stype]))
+    return cm.Symbol('{}{}{}'.format(str(symbol), magic_character, TYPE_SUFFIXES_INV[stype]))
 
 def Position(symbol):
     """Shorthand for creating a position symbol.
@@ -99,7 +101,7 @@ def erase_type(symbol):
     """
     st = get_symbol_type(symbol)
     if st != TYPE_UNKNOWN:
-        return cm.Symbol(str(symbol)[:-len(TYPE_SUFFIXES_INV[st])])
+        return cm.Symbol(str(symbol)[:-len(TYPE_SUFFIXES_INV[st])-len(magic_character)])
     return symbol
 
 def get_symbol_type(symbol):
@@ -110,7 +112,8 @@ def get_symbol_type(symbol):
     :return: Enum value of symbol's type
     :rtype: int
     """
-    return TYPE_SUFFIXES[str(symbol)[-2:]] if str(symbol)[-2:] in TYPE_SUFFIXES else TYPE_UNKNOWN
+    suffix = str(symbol).split(magic_character)[-1]
+    return TYPE_SUFFIXES[suffix] if suffix in TYPE_SUFFIXES else TYPE_UNKNOWN
 
 def DiffSymbol(symbol):
     """Returns the derivative symbol of the given symbol.
@@ -124,7 +127,7 @@ def DiffSymbol(symbol):
     s_type = get_symbol_type(symbol)
     if s_type == TYPE_UNKNOWN or s_type == TYPE_SNAP:
         raise CastException('Cannot generate derivative symbol for {}! The type is {}'.format(symbol, s_type))
-    return cm.Symbol('{}{}'.format(str(symbol)[:-2], TYPE_SUFFIXES_INV[s_type + 1]))
+    return create_symbol(erase_type(symbol), s_type + 1)
 
 def IntSymbol(symbol):
     """Returns the integral symbol of the given symbol.
@@ -138,4 +141,4 @@ def IntSymbol(symbol):
     s_type = get_symbol_type(symbol)
     if s_type == TYPE_UNKNOWN or s_type == TYPE_POSITION:
         raise CastException('Cannot generate integrated symbol for {}! The type is {}'.format(symbol, s_type))
-    return cm.Symbol('{}{}'.format(str(symbol)[:-2], TYPE_SUFFIXES_INV[s_type - 1]))
+    return create_symbol(erase_type(symbol), s_type - 1)
