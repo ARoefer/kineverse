04cae2e23bd2fb0f8af31464198d6ddeeaf5e0e3
Simon Stelter <stelter@uni-bremen.de>
2020-08-27 19:01:27 +0200

    added set type sets

--- src/kineverse/type_sets.py
+++ src/kineverse/type_sets.py
@@ -3,7 +3,7 @@ import kineverse.gradients.common_math as cm
 from kineverse.gradients.gradient_math   import GC, GM
 import numpy as np
 
-atomic_types = set([int, float, bool, str, GC, type(None)])
+atomic_types = set([int, float, bool, str, GC, type(None), set])
 
 matrix_types = {GM}.union(cm.matrix_types)
 symbolic_types = {GC, GM}.union(cm.math_types)
