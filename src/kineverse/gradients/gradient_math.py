86d71ea6226164bebfc3491d309fc2e95429da9b
Adrian Roefer <aroefer@uni-bremen.de>
2021-03-16 18:57:33 +0100

    Fixed unnecessary symengine import and added import of type erasure

--- src/kineverse/gradients/gradient_math.py
+++ src/kineverse/gradients/gradient_math.py
@@ -14,10 +14,11 @@ from kineverse.gradients.diff_logic         import Position, \
                                                    Snap, \
                                                    Symbol, \
                                                    DiffSymbol, \
-                                                   IntSymbol
+                                                   IntSymbol, \
+                                                   erase_type
 from kineverse.gradients.gradient_container import GradientContainer as GC, \
                                                    GradientMatrix    as GM 
-from kineverse.symengine_types              import symengine_types, symengine_matrix_types
+
 
 contrast = 1e10
 
@@ -63,6 +64,8 @@ if cm.SYM_MATH_ENGINE == 'CASADI':
             return cm.subs(expr.to_sym_matrix(), subs_dict)
         return cm.subs(expr, subs_dict)
 else:
+    from kineverse.symengine_types              import symengine_types, symengine_matrix_types
+    
     subs = cm.subs
 
 
