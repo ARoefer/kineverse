ca18886b1f20da442283b792d05a2d02712a9597
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2022-03-14 09:45:10 +0100

    Added parser for stringified casadi expressions

---
+++ src/kineverse/casadi_parser.py
@@ -0,0 +1,189 @@
+import string
+
+import kineverse.gradients.gradient_math as gm
+
+letters       = set(string.ascii_letters)
+digits        = set(string.digits)
+name_glyphs   = set(string.ascii_letters + string.digits + '_')
+number_glyphs = set(string.digits + '.')
+
+def op_add(a, b):
+    return a + b
+
+def op_sub(a, b):
+    return a - b
+
+def op_mul(a, b):
+    return a * b
+
+def op_div(a, b):
+    return a / b
+
+operators = {'+': op_add, '-': op_sub, '*': op_mul, '/': op_div}
+functions = {f.__qualname__: f for f in [getattr(gm, n) for n in dir(gm) if callable(getattr(gm, n))]}
+functions['sq'] = lambda x: x**2
+functions['fabs'] = gm.abs
+
+def parse(string):
+    # if string[:3] != 'SX(' or string[-1] == ')':
+    #     raise Exception('Casadi expressions need to start with "SX(" and end with ")". '
+    #                     f'The given expression starts with "{string[:3]}" and ends with "{string[-1]}"')
+    defs = {}
+
+    i = 0
+    while True:
+        i = consume_neutral(string, i)
+        if string[i] != '@':
+            break
+        n, d, i = parse_def(string, i, defs)
+        defs[n] = d
+        i = consume_neutral(string, i)
+        if string[i] != ',':
+            raise Exception(f'Expected "," after parsing of definition got "{string[i]}"')
+        i += 1
+
+    return parse_expr(string, i, defs)
+
+def parse_def(s, i, defs):
+    if s[i] != '@':
+        raise Exception(f'Definitions need to start with "@" but got "{s[i]}"')
+    
+    int_s, i = parse_int_str(s, i+1)
+    
+    if s[i] != '=':
+        raise Exception(f'Definition expected "=" but got "{s[i]}" during parsing of "@{int_s}"')
+    
+    i += 1
+    e, i = parse_expr(s, i, defs)
+    return f'@{int_s}', e, i
+
+def parse_matrix(s, i, defs):
+    m, i = parse_nested_list(s, i, defs)
+    return gm.Matrix(m), i
+
+def parse_nested_list(s, i, defs):
+    i = consume_neutral(s, i)
+    if s[i] == '[':
+        return parse_list(s, i, defs, inner_op=parse_nested_list)
+    else:
+        e, i = parse_expr(s, i, defs)
+        return e, i
+
+def parse_expr(s, i, defs):
+    terms = []
+    ops   = []
+    while True:
+        t, i = parse_term(s, i, defs)
+        terms.append(t)
+        i = consume_neutral(s, i)
+        if i == len(s) or s[i] not in '+-':
+            break
+        
+        ops.append(operators[s[i]])
+        i += 1
+    
+    acc = terms[0]
+    for o, t in zip(ops, terms[1:]):
+        acc = o(acc, t)
+    return acc, i
+
+def parse_list(s, i, defs, inner_op=parse_expr):
+    if s[i] not in '([':
+        raise Exception(f'Lists need to start with "(" or "[" but got "{s[i]}"')
+    sb = s[i]
+
+    l = []
+    while True:
+        e, i = inner_op(s, i + 1, defs)
+        l.append(e)
+        i = consume_neutral(s, i)
+        if s[i] != ',':
+            break
+    
+    if (sb == '(' and s[i] != ')') or (sb == '[' and s[i] != ']'):
+        raise Exception(f'List needs to close with ")" or "]", but got "{s[i]}"')
+    return l, i + 1
+
+def parse_term(s, i, defs):
+    factors = []
+    ops     = []
+    while True:
+        f, i = parse_factor(s, i, defs)
+        factors.append(f)
+        i = consume_neutral(s, i)
+        if i == len(s) or s[i] not in '*/':
+            break
+        
+        ops.append(operators[s[i]])
+        i += 1
+    
+    acc = factors[0]
+    for o, f in zip(ops, factors[1:]):
+        acc = o(acc, f)
+    return acc, i
+
+def parse_factor(s, i, defs):
+    i = consume_neutral(s, i)
+    sign, i = parse_sign(s, i)
+
+    if s[i] == '(':
+        e, i = parse_expr(s, i + 1, defs)
+        i = consume_neutral(s, i)
+        if s[i] != ')':
+            raise Exception(f'Expected ")" but got "{s[i]}" at index {i}')
+        return sign * e, i + 1
+    elif s[i] == '[':
+        m, i = parse_matrix(s, i, defs)
+        return sign * m, i
+    elif s[i] == '@':
+        int_s, i = parse_int_str(s, i + 1)
+        return defs[f'@{int_s}'], i
+    elif s[i] in letters:
+        name, i = parse_name(s, i)
+        if i < len(s) and s[i] == '(':
+            l, i = parse_list(s, i, defs)
+            if not name in functions:
+                raise Exception(f'Unknown function "{name}"')
+            return sign * functions[name](*l), i
+        return sign * gm.Symbol(name), i
+    else:
+        number, i = parse_number(s, i)
+        return sign * number, i
+
+def parse_sign(s, i):
+    return (-1, i+1) if s[i] == '-' else (1, i)
+
+def parse_number(s, i):
+    int_s, i = parse_int_str(s, i)
+    if i == len(s) or (s[i] not in '.e'):
+        return int(int_s), i
+    
+    decimal_s = '0'
+    if s[i] == '.':
+        decimal_s, i = parse_int_str(s, i + 1)
+    
+    if len(s) == i or s[i] != 'e':
+        return float(f'{int_s}.{decimal_s}'), i
+    
+    e_sign, i = parse_sign(s, i + 1)
+    exp_s, i  = parse_int_str(s, i)
+
+    return float(f'{int_s}.{decimal_s}e{e_sign * int(exp_s)}'), i
+
+def parse_int_str(s, i):
+    x = i
+    while i < len(s) and s[i] in digits:
+        i += 1
+    return s[x:i], i
+
+def parse_name(s, i):
+    x = i
+    while i < len(s) and s[i] in name_glyphs:
+        i += 1
+    return s[x:i], i
+
+def consume_neutral(s, i):
+    while i < len(s) and s[i] in {' ', '\t', '\n'}:
+        i += 1
+
+    return i
---
+++ test/test_casadi_parser.py
@@ -0,0 +1,67 @@
+import unittest as ut
+
+import kineverse.gradients.gradient_math  as gm
+import kineverse.casadi_parser            as cp
+
+class TestEventModel(ut.TestCase):
+
+    def test_consume_neutral(self):
+        self.assertEqual(cp.consume_neutral(' \n\ta', 0), 3)
+        self.assertEqual(cp.consume_neutral(' \n\t  ', 0), 5)
+
+    def test_parse_number(self):
+        self.assertEqual(cp.parse_number('0', 0)[0], 0)
+        self.assertEqual(cp.parse_number('24', 0)[0], 24)
+        self.assertEqual(cp.parse_number('24.75', 0)[0], 24.75)
+        self.assertEqual(cp.parse_number('24e3', 0)[0], 24e3)
+        self.assertEqual(cp.parse_number('24e-128', 0)[0], 24e-128)
+        self.assertEqual(cp.parse_number('24.75e-128', 0)[0], 24.75e-128)
+
+    def test_parse_name(self):
+        self.assertEqual(cp.parse_name('lol', 0)[0], 'lol')
+
+    def test_parse_factor(self):
+        defs = {'@12': 4}
+        self.assertEqual(cp.parse_factor(' -43', 0, defs)[0], -43)
+        self.assertEqual(cp.parse_factor('43', 0, defs)[0], 43)
+        self.assertEqual(cp.parse_factor('-43.32e4', 0, defs)[0], -43.32e4)
+        self.assertEqual(str(cp.parse_factor('  lol ', 0, defs)[0]), 'lol')
+        self.assertEqual(cp.parse_factor('  @12 ', 0, defs)[0], 4)
+        self.assertEqual(str(cp.parse_factor('sin(a)', 0, defs)[0]), str(gm.sin(gm.Symbol('a'))))
+
+    def test_parse_term(self):
+        defs = {'@12': 4}
+        self.assertEqual(cp.parse_term('@12*3/5-4', 0, defs)[0], defs['@12'] * 3 / 5)
+    
+    def test_parse_expr(self):
+        defs = {'@12': 4}
+        self.assertEqual(cp.parse_expr('33e-2+@12*3/5-4', 0, defs)[0], 33e-2 + defs['@12'] * 3 / 5 - 4)
+
+    def test_parse_list(self):
+        defs = {'@12': 4}
+        self.assertEqual(str(cp.parse_list('[ a, 423e3, @12,66]', 0, defs, cp.parse_expr)[0]), 
+                         str([gm.Symbol('a'), 423e3, defs['@12'], 66]))
+        
+    def test_parse_matrix(self):
+        defs = {'@12': 4}
+        self.assertEqual(cp.parse_matrix('32', 0, defs)[0], 32)
+        self.assertEqual(str(cp.parse_matrix('[32]', 0, defs)[0]), 
+                         str(gm.Matrix([32])))
+
+        M = gm.Matrix([[          1, 2, 3],
+                       [defs['@12'], 5, 6],
+                       [          7, 8, 9]])
+        self.assertEqual(str(cp.parse_matrix('[[1,2,3], [@12, 5, 6], [7, 8, 9]]', 0, defs)[0]),
+                         str(M))
+
+    def test_parse(self):
+        defs = {'@1': 4, '@2': gm.sin(gm.Symbol('a'))}
+        ref_1 = gm.cm.ca.SX(defs['@1'])
+        M = gm.Matrix([[    1, 2, ref_1],
+                       [ref_1, 5, defs['@2']],
+                       [    7, 8, defs['@2']]])
+        self.assertEqual(str(cp.parse('@1=4, @2=sin(a), ([[1,2,@1], [@1, 5, @2], [7, 8, @2]])')[0]),
+                         str(M))
+
+if __name__ == '__main__':
+    ut.main()
